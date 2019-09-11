658e7b8d40cfeee2e0f28b2756aaf0e904017c1a
troydaniels <troyaadaniels@gmail.com>
2019-09-11 17:00:58 +1000

    Refactor header styling, add DateRangePicker

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 import '../styles/Header.css';
+import logo from '../images/coreplus_logo.png';
 
 @observer
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: [new Date(), new Date()],
+        };
+    }
+
+    onChange = date => this.setState({ date });
+
     render() {
         return (
             <div className="header-container">
-
+                <div className="logo-container">
+                    <img src={logo} alt="Coreplus Logo" width="250" />
+                </div>
+                <div className="report-details-container">
+                    <div className="report-controls-container">
+                        <div className="date-range-text">REPORT FOR</div>
+                        <DateRangePicker
+                            className="date-range-picker"
+                            onChange={this.onChange}
+                            value={this.state.date}
+                        />
+                    </div>
+                    <div className="report-title">
+                        PRACTITIONER FINANCIAL REPORTS
+                    </div>
+                </div>
             </div>
         );
     }
